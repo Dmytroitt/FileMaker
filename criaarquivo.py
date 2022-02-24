e2aa5f5423f37953a890476beeb35b215024a4a9
Hojako <89229676+Hojako@users.noreply.github.com>
2022-02-24 17:56:33 -0400

    Update criaarquivo.py

--- criaarquivo.py
+++ criaarquivo.py
@@ -1,12 +1,12 @@
 import os
 print('#+'*12)
-print('Criador de arquivos')
+print('File maker')
 print('#+'*12)
-nomeArq = str(input('Nome dos arquivos: '))
-extencArq = str(input('Extenção[EX: py,txt]: '))
-quantArq = int(input('Quantidade de arquivos: '))
+nomeArq = str(input('File names: '))
+extencArq = str(input('Extension[EX: py,txt]: '))
+quantArq = int(input('Number of files: '))
 contador = 0
 for c in range(0,quantArq):
 	os.system(f'touch {nomeArq}{c}.{extencArq}')
 	contador = contador + 1
-	print(f'{contador} arquivos criados')
+	print(f'did {contador} files')
